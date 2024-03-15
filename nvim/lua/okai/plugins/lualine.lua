e345bd3d0a823edff4f920066410aeb1b3f7d58d
okaihe <kai.herbst@online.de>
2024-03-15 23:19:21 +0100

    Update filetree

--- nvim/lua/okai/plugins/lualine.lua
+++ nvim/lua/okai/plugins/lualine.lua
@@ -5,8 +5,10 @@ return {
             options = {
                 icons_enabled = true,
                 theme = "auto",
+
                 component_separators = { left = "", right = "" },
                 section_separators = { left = "", right = "" },
+
                 disabled_filetypes = {
                     statusline = { "NvimTree", "neo-tree" },
                     winbar = {},
