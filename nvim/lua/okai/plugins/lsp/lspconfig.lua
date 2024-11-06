2c5ac835f99614213c4aa115133338688a2df55c
= <kai.herbst@deutschebahn.com>
2024-11-06 11:53:05 +0100

    Update python black formatter

--- nvim/lua/okai/plugins/lsp/lspconfig.lua
+++ nvim/lua/okai/plugins/lsp/lspconfig.lua
@@ -162,5 +162,10 @@ return {
             capabilities = capabilities,
             on_attach = on_attach,
         })
+
+        lspconfig["pylint"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+        })
     end,
 }
