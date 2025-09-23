a220ec4df354597788195ca0765a5903dc9447ae
okaihe <kai.herbst@online.de>
2025-09-23 23:37:53 +0200

    Update gitsignes

--- nvim/lua/okai/plugins/gitsignes.lua
+++ nvim/lua/okai/plugins/gitsignes.lua
@@ -2,22 +2,23 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function()
         require("gitsigns").setup({
-            -- signs = {
-            -- 	add = { text = "+" },
-            -- 	change = { text = "~" },
-            -- 	delete = { text = "-" },
-            -- 	topdelete = { text = "‾" },
-            -- 	changedelete = { text = "/" },
-            -- 	untracked = { text = "┆" },
-            -- },
             signs = {
-                add = { text = "▍" },
-                change = { text = "▍" },
-                delete = { text = "▍" },
-                topdelete = { text = "▍" },
-                changedelete = { text = "▍" },
-                untracked = { text = "▍" },
+                add = { text = "┃" },
+                change = { text = "┃" },
+                delete = { text = "_" },
+                topdelete = { text = "‾" },
+                changedelete = { text = "~" },
+                untracked = { text = "┆" },
             },
+            signs_staged = {
+                add = { text = "┃" },
+                change = { text = "┃" },
+                delete = { text = "_" },
+                topdelete = { text = "‾" },
+                changedelete = { text = "~" },
+                untracked = { text = "┆" },
+            },
+            signs_staged_enable = true,
             signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
             numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
             linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
@@ -25,37 +26,29 @@ return {
             watch_gitdir = {
                 follow_files = true,
             },
-            attach_to_untracked = true,
+            auto_attach = true,
+            attach_to_untracked = false,
             current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
             current_line_blame_opts = {
                 virt_text = true,
                 virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
                 delay = 1000,
                 ignore_whitespace = false,
+                virt_text_priority = 100,
+                use_focus = true,
             },
-            current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+            current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
             sign_priority = 6,
             update_debounce = 100,
             status_formatter = nil, -- Use default
             max_file_length = 40000, -- Disable if file is longer than this (in lines)
             preview_config = {
                 -- Options passed to nvim_open_win
-                border = "single",
                 style = "minimal",
                 relative = "cursor",
                 row = 0,
                 col = 1,
             },
         })
-
-        local gs = package.loaded.gitsigns
-        local keymap = vim.keymap
-        keymap.set("n", "<leader>lb", gs.toggle_current_line_blame, { desc = "Toggle line blame" })
-        keymap.set("n", "<leader>hp", gs.preview_hunk, { desc = "Preview hunk" })
-        keymap.set("n", "<leader>hd", gs.diffthis, { desc = "Diff this" })
-        keymap.set("n", "<leader>td", gs.toggle_deleted, { desc = "Toggle deleted" })
-        keymap.set("n", "<leader>hb", function()
-            gs.blame_line({ full = true })
-        end, { desc = "Blame line full" })
     end,
 }
